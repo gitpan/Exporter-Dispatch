081b3e648e3a2df5a1a5564d3f100ea9c3d7e90f
Joseph F. Ryan <ryan.311@osu.edu>
2004-07-09 21:02:05 +0000

    Import of JRYAN/Exporter-Dispatch-2.10 from CPAN.
    gitpan-cpan-distribution: Exporter-Dispatch
    gitpan-cpan-version:      2.10
    gitpan-cpan-path:         JRYAN/Exporter-Dispatch-2.10.tar.gz
    gitpan-cpan-author:       JRYAN
    gitpan-cpan-maturity:     released

--- lib/Exporter/Dispatch.pm
+++ lib/Exporter/Dispatch.pm
@@ -1,17 +1,17 @@
 package Exporter::Dispatch;
 use Carp qw(croak);
-our $VERSION = 2.00;
+our $VERSION = 2.10;
 
 sub import {
     my $pkg = (caller)[0];
     if (@_ > 2) {
         croak 'Incorrect import list for Exporter::Dispatch';
     }
-    elsif ($_[1] eq 'create_dptable') {
+    elsif ($_[-1] eq 'create_dptable') {
         *{"${pkg}::create_dptable"} = \&create_dptable;
         return
     }
-    elsif ($_[1] eq 'dptable_alias') {
+    elsif ($_[-1] eq 'dptable_alias') {
         *{"${pkg}::dptable_alias"} = sub {
             *{"${pkg}::$_[1]"} = *{"${pkg}::$_[0]"}
         }
@@ -80,22 +80,22 @@ Simple and modular creation of dispatch tables.
 
 =head1 DESCRIPTION
 
-    Dispatch tables are great and convienient, but sometimes can be a bit of a
-    pain to write. You have references flying over here and closures flying over
-    there; yuck!  Thats much too complicated for so simple of an idea. Wouldn't
-    it be great if you could say "Ok, I have a set of subs here, and I want a
-    dispatch table that maps each subname to each sub... Go do it, Perl genie!"
-    With this short snippet of a module, now you can. Just throw your subs in a
-    module, C<use Exporter::Dispatch;>, and a C<create_dptable> subroutine that
-    (surpise!) creates a dispatch table that maps each subname in the package to
-    its corresponding sub will magically appear to serve you.
-    
-    In a more serious tone, C<Exporter::Dispatch> essentially creats a
-    subroutine (named create_dptable) in namespaces it is imported to.  This
-    subroutine, when called, returns a hashref that maps a string of each
-    subname to the corresponding subroutine.  Subroutines that begin with an
-    underscore are not added to the returned table, so they can be used as
-    "helper" routines.
+Dispatch tables are great and convienient, but sometimes can be a bit of a
+pain to write. You have references flying over here and closures flying over
+there; yuck!  Thats much too complicated for so simple of an idea. Wouldn't
+it be great if you could say "Ok, I have a set of subs here, and I want a
+dispatch table that maps each subname to each sub... Go do it, Perl genie!"
+With this short snippet of a module, now you can. Just throw your subs in a
+module, C<use Exporter::Dispatch;>, and a C<create_dptable> subroutine that
+(surpise!) creates a dispatch table that maps each subname in the package to
+its corresponding sub will magically appear to serve you.
+
+In a more serious tone, C<Exporter::Dispatch> essentially creats a
+subroutine (named create_dptable) in namespaces it is imported to.  This
+subroutine, when called, returns a hashref that maps a string of each
+subname to the corresponding subroutine.  Subroutines that begin with an
+underscore are not added to the returned table, so they can be used as
+"helper" routines.
     
 =head2 Exports
     
@@ -139,7 +139,7 @@ http://search.cpan.org/author/JRYAN/.
 
 =head1 VERSION
 
-This document describes version 2.00 of Exporter::Dispatch.
+This document describes version 2.10 of Exporter::Dispatch.
 
 =head1 AUTHOR
 
@@ -147,7 +147,7 @@ Joseph F. Ryan <ryan.311@osu.edu>
 
 =head1 COPYRIGHT
 
-Copyright 2003 Joseph F. Ryan. All rights reserved.
+Copyright 2004 Joseph F. Ryan. All rights reserved.
 
 This library is free software; you can redistribute it and/or modify it under
 the same terms as Perl itself.
